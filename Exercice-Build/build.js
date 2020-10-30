02173d292dd560778f0303e707a55fb68748d075
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-10-30 16:57:42 +0100

    Fin de formation

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -1,8 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
-const del = require('del');
 const md5 = require('md5');
-const UglifyJS = require('uglify-es');
+const Terser = require('terser');
 
 const distPath = path.resolve(__dirname, 'dist');
 const srcPath = path.resolve(__dirname, 'src');
@@ -12,9 +11,43 @@ const indexHtmlPath = path.resolve(srcPath, 'index.html');
 const indexHtmlDistPath = path.resolve(distPath, 'index.html');
 const appJsDistPath = path.resolve(distPath, 'app.js');
 
+async function rmAndMkdir() {
+  await fs.remove(distPath);
+  await fs.mkdir(distPath);
+}
+
+async function buildJs() {
+  const buffers = await Promise.all([
+    fs.readFile(horlogeJsPath),
+    fs.readFile(indexJsPath),
+  ]);
+
+  await fs.writeFile(appJsDistPath, Buffer.concat(buffers));
+}
+
+async function buildHtml() {
+  let content = await fs.readFile(indexHtmlPath, { encoding: 'utf-8' });
+
+  content = content.replace(/<script.*<\/script>/s, '<script src="app.js"></script>');
+
+  await fs.writeFile(indexHtmlDistPath, content);
+}
+
 async function build() {
   // ATTENTION avec fs-extra pas de fs.promises.
-  await fs.remove(distPath);
+  try {
+    await rmAndMkdir();
+    // await buildJs();
+    // await buildHtml();
+    await Promise.all([
+      buildJs(),
+      buildHtml()
+    ]);
+    console.log('build done');
+  }
+  catch(err) {
+    console.log(err);
+  }
 }
 
 build();
